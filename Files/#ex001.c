700435fef3c027ebe68512b0ce8842b78772af7d
Gustavo Santos <63077870+Poitt@users.noreply.github.com>
2021-03-13 22:27:21 -0300

    Update #ex001.c

--- Files/#ex001.c
+++ Files/#ex001.c
@@ -1,6 +1,6 @@
 /*The registration of an address book,names, age and telephone numbers of 5 people, complements an appropriate registration structure in C, that performs the following tasks: 
 A - registration of information: name, address, telephone number and age. B - age research. C - alphabetical classification. 
-D  - alteration of a record that occorred with an error. E - exit the program. */
+D  - alteration of a record that occurred with an error. E - exit the program. */
 
 #include <stdio.h> // biblioteca padrao de entrada e saida
 #include <unistd.h> // biblioteca para sleep
